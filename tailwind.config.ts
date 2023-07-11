49de6d5769f477b4d875cb5fd6be76bf25571fdd
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-11 18:06:06 +0400

    add heading styles to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -16,7 +16,46 @@ module.exports = {
       },
     },
     extend: {
+      fontSize: {
+        "heading-xl": [
+          "5.5rem",
+          {
+            lineHeight: "5.5rem",
+            fontWeight: "700",
+            letterSpacing: "-0.1563rem",
+          },
+        ],
+        "heading-l": [
+          "3rem",
+          {
+            lineHeight: "3.5rem",
+            fontWeight: "700",
+            letterSpacing: "-0.0938rem",
+          },
+        ],
+        "heading-m": [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+            fontWeight: "700",
+          },
+        ],
+        body: [
+          "1.125rem",
+          {
+            lineHeight: "1.75rem",
+            fontWeight: "500",
+          },
+        ],
+      },
+      fontFamily: {
+        grotesk: "var(--font-grotesk)",
+      },
       colors: {
+        "almost-black": "hsl(var(--almost-black))",
+        "light-green": "hsl(var(--light-green))",
+        "dark-gray": "hsl(var(--dark-gray))",
+        "light-gray": "hsl(var(--light-gray))",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -73,4 +112,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
