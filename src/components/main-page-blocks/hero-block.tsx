1933f924161cf638d16c3bae6e0248ea969b5c02
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-13 18:47:16 +0400

    import images using fs

--- src/components/main-page-blocks/hero-block.tsx
+++ src/components/main-page-blocks/hero-block.tsx
@@ -1,8 +1,29 @@
-import { motion, useScroll, useSpring } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { motion, useScroll } from "framer-motion";
+import path from "path";
+import { promises as fs } from "fs";
+import Image from "next/image";
+
+async function getImageFilenames() {
+  // using process.cwd() to get the path
+  const imageDirectory = path.join(process.cwd(), "/public/main-block-images");
+  // fs.readdir to get all files in directory
+  const imageFilenames = await fs.readdir(imageDirectory);
+  console.log(imageFilenames);
+  return imageFilenames;
+}
 
 export function HeroBlock() {
   const { scrollYProgress } = useScroll();
-  //   const scaleX = useSpring(scrollYProgress);
+  const [imageFilenames, setImageFilenames] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (imageFilenames.length === 0)
+      void getImageFilenames().then((filenames) =>
+        setImageFilenames(filenames)
+      );
+  }, [imageFilenames.length]);
+
   return (
     <>
       <motion.div
@@ -13,5 +34,19 @@ export function HeroBlock() {
         <code>useScroll</code> demo
       </h1>
     </>
+  );
+}
+
+function ScrollImage({ filename }: { filename: string }) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  //   const y = useParallax(scrollYProgress, 300);
+
+  return (
+    <section>
+      <div ref={ref}>
+        <Image src={`${filename}`} alt="my image" />
+      </div>
+    </section>
   );
 }
