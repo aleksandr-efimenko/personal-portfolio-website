5224d7d37cf863ba4a0af51f613f9550b23daf18
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-14 12:20:17 +0400

    update slide images

--- src/components/main-page-blocks/hero-block-images.tsx
+++ src/components/main-page-blocks/hero-block-images.tsx
@@ -3,5 +3,7 @@ import Img2 from "~/main-block-images/2.png";
 import Img3 from "~/main-block-images/3.png";
 import Img4 from "~/main-block-images/4.png";
 import Img5 from "~/main-block-images/5.png";
+import Img6 from "~/main-block-images/6.png";
+import Img7 from "~/main-block-images/7.png";
 
-export const heroBlockImages = [Img1, Img2, Img3, Img4, Img5];
+export const heroBlockImages = [Img1, Img2, Img3, Img4, Img5, Img6, Img7];
