726f863cfeb941b1d89709b36353a9bd02226cc1
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-15 20:43:21 +0400

    create empty projects block

--- src/components/main-page-blocks/projects-block.tsx
+++ src/components/main-page-blocks/projects-block.tsx
@@ -1,3 +1,55 @@
+import { StyledLink } from "../ui/styled-link";
+
+const projectsData = [
+  {
+    title: "Kanban Desk",
+    tags: [
+      "React",
+      "TypeScript",
+      "Next.js",
+      "Tailwind CSS",
+      "MongoDB",
+      "Prisma",
+    ],
+    href: "https://kanban-desk.alexefimenko.com/.vercel.app/",
+    projectPage: "/projects/kanban-board",
+    link: "https://kanban-board.alexefimenko.com/",
+    github: "https://github.com/aleksandr-efimenko/kanban-task-management",
+    image: "/images/kanban-board.png",
+  },
+  {
+    title: "Portfolio",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind CSS"],
+    href: "https://alexefimenko.com/",
+    projectPage: "/projects/portfolio",
+    link: "https://alexefimenko.com/",
+    github: "",
+    image: "/images/portfolio.png",
+  },
+  {
+    title: "Weather App",
+    tags: [
+      "React",
+      "TypeScript",
+      "Next.js",
+      "Tailwind CSS",
+      "OpenWeatherMap API",
+    ],
+    href: "https://weather-app.alexefimenko.com/",
+    projectPage: "/projects/weather-app",
+    link: "https://weather-app.alexefimenko.com/",
+    github: "",
+  },
+];
+
 export function ProjectsBlock() {
-  return <div>projects-block</div>;
+  return (
+    <div>
+      <div className="flex w-full items-center justify-between">
+        <h2 className="text-heading-l">Projects</h2>
+        <StyledLink href={"/"}>Contact me</StyledLink>
+      </div>
+      <div className="grid-cols-2"></div>
+    </div>
+  );
 }
