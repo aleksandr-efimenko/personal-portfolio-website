780d37348a183a8cca3f16d5a4c7017c231a095f
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-18 14:35:25 +0400

    add responsiveness to the contact block, add bg image

--- src/components/main-page-blocks/contact-block.tsx
+++ src/components/main-page-blocks/contact-block.tsx
@@ -1,11 +1,20 @@
 import { ContactForm } from "../contact-form";
+import Image, { type StaticImageData } from "next/image";
+import patternDots from "~/pattern-dots.svg";
 
 const blockDescription = `I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.`;
 
 export function ContactBlock() {
   return (
-    <div className="container flex gap-6">
-      <div className="flex flex-1 flex-col gap-9">
+    <div className="container relative flex flex-col justify-between gap-16 md:flex-row md:gap-24 lg:gap-48">
+      <Image
+        src={patternDots as StaticImageData}
+        alt="pattern circle"
+        className="absolute -left-32 z-10 opacity-20 invert "
+        height={300}
+      />
+
+      <div className="flex  flex-1 flex-col gap-9">
         <h3 className="text-heading-xl">Contact</h3>
         <p className="text-body">{blockDescription}</p>
       </div>
