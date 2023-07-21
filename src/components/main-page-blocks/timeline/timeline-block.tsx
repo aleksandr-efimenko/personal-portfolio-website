c34a8311a64a3ed12db8cbfbc96e186676addebf
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-21 17:50:50 +0400

    add timeline info

--- src/components/main-page-blocks/timeline/timeline-block.tsx
+++ src/components/main-page-blocks/timeline/timeline-block.tsx
@@ -1,10 +1,71 @@
 import { Icons } from "@/components/icons";
-import { StarIcon } from "@radix-ui/react-icons";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+
+const timelineElements = [
+  {
+    title: "C# Software Engineer",
+    subtitle: "Self-employed, Saint Petersburg, Russia",
+    date: "March 2016 – Present",
+    icon: <Icons.work />,
+    description: "",
+    bulletPoints: [
+      "Developed from scratch a system that allows to collect and analyze information about all domain names in the .ru zone (about 5,000,000 domain names)",
+      "The system includes an MS SQL database (more than 50 relational tables), a .Net core 3.1 Razor pages web application for managing and monitoring, about 10 docker containers for collecting data independently from external APIs and parsing websites by Selenium web driver.",
+      "The web application includes forms for CRUD operations processed by Entity Framework, API interface for connecting with the docker containers and transfer data by JSON, dashboard with main metrics and built-in authentication service.",
+    ],
+    mainDuties: [],
+  },
+  {
+    title: "IT Consultant",
+    subtitle:
+      "Self-employed, Joint-Stock Company “Project Development Institute No.1”, Saint Petersburg, Russia",
+    date: "September 2017 - August 2019",
+    icon: <Icons.work />,
+    description:
+      "Successfully provided advice on information systems issues and modified to customer needs digital document management software for an organization with 100 employees.",
+    bulletPoints: [],
+    mainDuties: [
+      "Information support, provision of consultations on the installation and configuration of digital document management “1C Document Management”",
+      "Conducting technical studies at the request of the users on modification and improvement of the system",
+      "Testing and implementing information systems business solutions, quality control, stability and system availability and updating the information base",
+      "Implementing maintenance procedures to maximize the efficiency of the information system",
+    ],
+  },
+  {
+    title: "Lead Engineer",
+    subtitle:
+      "Joint-Stock Company “Project Development Institute No.1”, Saint Petersburg, Russia",
+    date: "May 2015 - August 2017",
+    icon: <Icons.work />,
+    description: "",
+    mainDuties: [
+      "Implemented and modified to customer needs digital document management software for 4 organizations with a number of employees from 20 to 150.",
+      "This software reduced the average time for approval of documentation within the organization from 7 to 2 days because of role-based routes, made generating new documents more than 3 times faster because of using pre-installed templates, and made searching for a document instantaneous.",
+      "Main duties included “1C Document Management” setup, maintenance and development business solutions using 1C programming language embedded in the “1C Enterprise” platform. Managing, monitoring and maintaining MS SQL Server databases, ensuring database security, stability and system availability. Maintaining databases backup and recovery infrastructure",
+    ],
+  },
+  {
+    title: "Technical Support Engineer",
+    subtitle:
+      "FGKU North-West territorial administration of the property relations, Saint Petersburg, Russia",
+    date: "December 2010 - April 2015",
+    icon: <Icons.work />,
+    description: "",
+    bulletPoints: [
+      "Made a significant contribution to increasing the productivity of the organization by proactive monitoring, optimizing and upgrading hardware, software and related computer equipment",
+      "Implemented and customized digital document management system “E1 Evfrat”, which helped the company boost cooperation between all departments and optimized the workload of the staff",
+    ],
+    mainDuties: [
+      "Research, diagnosis, troubleshooting, and resolving customer issues with software and hardware",
+      "Maintaintanance and co-ordination local and global networks hardware, software and related computer equipment",
+      "Installation, maintenance, troubleshooting and upgrading database server hardware and software",
+    ],
+  },
+];
 
 export function TimelineBlock() {
   return (
@@ -12,98 +73,37 @@ export function TimelineBlock() {
       <h2 className="text-heading-xl">Timeline</h2>
 
       <VerticalTimeline layout="1-column-left">
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="2011 - present"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<Icons.work />}
-        >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2010 - 2011"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<Icons.work />}
-        >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2008 - 2010"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<Icons.work />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Los Angeles, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2006 - 2008"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<Icons.work />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="April 2013"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={<Icons.education />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Content Marketing for Web, Mobile and Social Media
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>Strategy, Social Media</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="November 2012"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={<Icons.education />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Agile Development Scrum Master
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>Creative Direction, User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={<Icons.education />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Bachelor of Science in Interactive Digital Media Visual Imaging
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor Degree
-          </h4>
-          <p>Creative Direction, Visual Design</p>
-        </VerticalTimelineElement>
+        {timelineElements.map((element, index) => (
+          <VerticalTimelineElement
+            key={index}
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+            date={element.date}
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            icon={element.icon}
+          >
+            <h3 className="vertical-timeline-element-title">{element.title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              {element.subtitle}
+            </h4>
+            {element.description && <p>{element.description}</p>}
+            {element.bulletPoints && (
+              <ul>
+                {element.bulletPoints.map((bulletPoint, index) => (
+                  <li key={index}>{bulletPoint}</li>
+                ))}
+              </ul>
+            )}
+            {element.mainDuties && (
+              <ul>
+                {element.mainDuties.map((mainDuty, index) => (
+                  <li key={index}>{mainDuty}</li>
+                ))}
+              </ul>
+            )}
+          </VerticalTimelineElement>
+        ))}
       </VerticalTimeline>
     </div>
   );
