933136772fa68b6a0e18981111b933c03533dda5
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-23 21:57:46 +0400

    fix eslint warnings

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import type * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
 import {
-  type Controller,
+  Controller,
   type ControllerProps,
   type FieldPath,
   type FieldValues,
