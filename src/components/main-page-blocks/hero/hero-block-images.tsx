06f1197eb808c708d0e3c0feec198aaf6ae18a12
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-23 22:00:28 +0400

    remove not in use images

--- src/components/main-page-blocks/hero/hero-block-images.tsx
+++ src/components/main-page-blocks/hero/hero-block-images.tsx
@@ -1,9 +1,4 @@
 import Img1 from "~/main-block-images/1.png";
 import Img2 from "~/main-block-images/2.png";
-import Img3 from "~/main-block-images/3.png";
-import Img4 from "~/main-block-images/4.png";
-import Img5 from "~/main-block-images/5.png";
-import Img6 from "~/main-block-images/6.png";
-import Img7 from "~/main-block-images/7.png";
 
 export const heroBlockImages = [Img1, Img2];
