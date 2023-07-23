edfefdad217663673bab3ad416eaddeedaf6b2b1
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-23 21:13:16 +0400

    add logo animation

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link
-      href="/"
-      className="group w-fit whitespace-nowrap text-heading-m lowercase transition-colors hover:text-light-green "
-    >
-      {siteConfig.logo}
-      <span className="text-light-green transition-colors group-hover:text-foreground">
-        .com
-      </span>
-    </Link>
+    <div className="animate-charcter">
+      <Link
+        href="/"
+        className="group w-fit whitespace-nowrap text-heading-m lowercase transition-colors hover:text-light-green "
+      >
+        {siteConfig.logo}
+        <span className="text-light-green transition-colors group-hover:text-foreground">
+          .com
+        </span>
+      </Link>
+    </div>
   );
 }
