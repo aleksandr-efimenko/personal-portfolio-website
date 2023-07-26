ea3103f4eaa3d44e1a072443f14c8e8ef50c3979
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-26 20:25:03 +0400

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,18 @@ import { api } from "@/utils/api";
 import "@/styles/globals.css";
 import { RootLayout } from "@/components/layout/layout";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </ThemeProvider>
+      <Analytics />
+    </>
   );
 };
 
